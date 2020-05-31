20674f9587265c9aa45a54d9d1de03663b48e920
John <likisongproduct@gmail.com>
2020-05-30 19:58:15 -0700

    added sidebar component with partial add category functionality

--- client/src/sections/Fridge/Sidebar/Sidebar.tsx
+++ client/src/sections/Fridge/Sidebar/Sidebar.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+ 
 
 import './Sidebar.css';
 
 export default function Sidebar() {
+
+  const [categories, setCategories] = useState([
+    "Meats", "Fruits", "Veggies"
+  ])
+
   return (
       <div className='sidebar'>
-            Sidebar placeholder
+        {categories.map( i => (
+          <div className="category" key={i}>{i}</div>
+          )
+        )}
+        <div className="category" onClick={() => setCategories(categories => [...categories, "apple"])}>+</div>
       </div>
   );
 }
+
