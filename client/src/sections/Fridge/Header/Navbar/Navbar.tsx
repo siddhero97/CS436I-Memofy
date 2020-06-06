18609bb8809a5c82182508fdbe40c1a69e3ecf20
Kwan <kwan,.lam@alumni.ubc.ca>
2020-06-05 23:22:23 -0700

    Create Navbar component

--- client/src/sections/Fridge/Header/Navbar/Navbar.tsx
+++ client/src/sections/Fridge/Header/Navbar/Navbar.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import {Button, ButtonGroup} from '@shopify/polaris';
 
 import './Navbar.css';
 
 export default function Navbar() {
+  const history = useHistory();
+
+  const handleHomeClick = () => {
+    history.push('./home');
+  }
+
+  const handleLogoutClick = () => {
+    history.push('./login');
+  }
+
   return (
       <div className='navbar'>
-            Navbar placeholder
+        <ButtonGroup fullWidth>
+          <div className='button'>
+            <Button primary onClick={handleHomeClick}>Home</Button>
+          </div>
+          <div className='button'>
+            <Button disabled>WIP</Button>
+          </div>
+          <div className='button'>
+            <Button disabled>WIP</Button>
+          </div>
+          <div className='button'>
+            <Button disabled>WIP</Button>
+          </div>
+          <div className='button'>
+            <Button disabled>WIP</Button>
+          </div>
+          <div className='button'>
+            <Button disabled>WIP</Button>
+          </div>
+          <div className='button'>
+            <Button destructive onClick={handleLogoutClick}>Logout</Button>
+          </div>
+        </ButtonGroup>
       </div>
   );
 }
