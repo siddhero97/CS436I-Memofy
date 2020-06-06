9e63bb07bc3bd6c01cfea27caaddb83aed795ff0
Kwan <kwan,.lam@alumni.ubc.ca>
2020-06-06 10:57:14 -0700

    Enhance ItemList to support adding items

--- client/src/sections/Fridge/ItemList/ItemList.tsx
+++ client/src/sections/Fridge/ItemList/ItemList.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Card, Button} from '@shopify/polaris';
+import React, { useState, useCallback } from 'react';
+import {Card, Button, Modal, TextField} from '@shopify/polaris';
 import {Item} from './components';
 
 import './ItemList.css';
@@ -9,7 +9,7 @@ interface Item {
   expiry: string;
 }
 
-const items: Item[] = [
+const mockItems: Item[] = [
   {
     name: 'Apples',
     expiry: '05/02/2020',
@@ -40,7 +40,30 @@ const items: Item[] = [
   }
 ]
 
+// NEED TO REFACTOR
 export default function ItemList() {
+  const [showModal, setShowModal] = useState(false);
+  const [items, setItems] = useState(mockItems);
+  const [value, setValue] = useState('');
+
+  const toggleShowModal = useCallback(() => {
+    setValue('');
+    setShowModal(!showModal);
+  }, [showModal]);
+
+  const handleTextChange = useCallback((newValue) => {
+    setValue(newValue);
+  }, []);
+    
+  const handleAddFoodItem = useCallback(() => {
+    const newItem = {
+      name: value,
+      expiry: '05/02/2020',
+    }
+
+    setItems([...items, newItem]);
+    toggleShowModal();
+  }, [items, value, toggleShowModal]);
 
   const itemListMarkup = items.map(({name, expiry}, id) => {
     return (
@@ -56,7 +79,26 @@ export default function ItemList() {
         <div className='item'>
           <Card sectioned>
             <Card.Section title="Can't find your item?">
-              <Button fullWidth primary>Add Item</Button>
+              <Button fullWidth primary onClick={toggleShowModal}>Add Item</Button>
+              <Modal
+                open={showModal}
+                onClose={toggleShowModal}
+                title='Add new food item'
+                primaryAction={{
+                  content: 'Add',
+                  onAction: handleAddFoodItem,
+                }}
+                secondaryActions={[
+                  {
+                    content: 'Cancel',
+                    onAction: toggleShowModal,
+                  }
+                ]}
+              >
+                <Modal.Section>
+                  <TextField label="Food name" value={value} onChange={handleTextChange} />
+                </Modal.Section>
+              </Modal>
             </Card.Section>
           </Card>
         </div>
