fc6b019ae4c5bb95e6dde4015d42263f84621c03
Kwan <kwan,.lam@alumni.ubc.ca>
2020-06-07 16:17:51 -0700

    Create Item actions for add/delete

---
+++ client/src/store/item/actions.ts
@@ -0,0 +1,15 @@
+import {Item, ItemActionTypes, ADD_ITEM, DELETE_ITEM} from './types';
+
+export function addItem(newItem: Item): ItemActionTypes {
+  return {
+    type: ADD_ITEM,
+    payload: newItem,
+  };
+}
+
+export function deleteItem(id: number): ItemActionTypes {
+  return {
+    type: DELETE_ITEM,
+    payload: id,
+  };
+}
