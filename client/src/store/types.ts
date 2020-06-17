b3c91de084bb0a4a9454bd6146d44f3c66361283
Vincent Chiang <vincentchiang1@gmail.com>
2020-06-17 11:42:57 -0700

    redux user state

--- client/src/store/types.ts
+++ client/src/store/types.ts
@@ -2,8 +2,13 @@ import {Item} from "./item/types";
 
 export type RootState = {
   item: ItemState;
+  user: UserState;
 };
 
 export interface ItemState {
   items: Item[];
+}
+
+export interface UserState {
+  email: string | null;
 }
