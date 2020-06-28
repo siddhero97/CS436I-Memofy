35887cdd299edb18699432ba22c975f26c9ee0a9
John <likisongproduct@gmail.com>
2020-06-27 22:54:14 -0700

    updated login page

--- client/src/sections/Login/Login.tsx
+++ client/src/sections/Login/Login.tsx
@@ -1,6 +1,7 @@
 import React, {useState, useCallback} from 'react';
 import {Button, Card, Heading, ButtonGroup} from '@shopify/polaris';
 import logo from 'icons/logo.png';
+import example from '../example.png';
 
 import './Login.css';
 import LoginModal from './LoginModal/LoginModal';
@@ -16,20 +17,21 @@ export default function Login() {
         <div className="title">Memofy</div>
       </div>
 
-      {/* <Card>
-        <div className='login-heading'>
-          <Heading element='h1'>Memofy</Heading>
-          <img src={logo} className="logo" alt={'Not found'} />
+      <div className="main-page">
+        <div className="text-content">
+          <div className="intro">Keep an ease of mind</div>
+          <h1 className="main-content">One Fridge, Two Fridge, Shared Fridge, Memofy Tracks it all. </h1>  
+          <div className="additional-content">Let Memofy keep track of your food, so you can focus on enjoying it. </div>
+          <ButtonGroup fullWidth>
+              <Button primary onClick={toggleShowModal} fullWidth={false}>Login</Button>
+              <Button outline onClick={toggleShowModal}>Register</Button>
+          </ButtonGroup>
         </div>
-        <Card sectioned>
-          <div className="login-button-group">
-            <ButtonGroup fullWidth>
-              <Button primary onClick={toggleShowModal}>Login</Button>
-            </ButtonGroup>
-          </div>
-        </Card>
-      </Card>
-      <LoginModal active={showModal} handleChange={toggleShowModal} /> */}
+        <div className="example-content">
+          <img src={example} className="app-example" alt={'Not found'} />
+        </div>
+      </div>
+      <LoginModal active={showModal} handleChange={toggleShowModal} />
     </div>
   );
 }
