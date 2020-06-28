1e7b6adcd45c3d160dffd88a7ffd81e254264709
John <likisongproduct@gmail.com>
2020-06-27 23:08:45 -0700

    added register modal

--- client/src/sections/Login/Login.tsx
+++ client/src/sections/Login/Login.tsx
@@ -1,14 +1,18 @@
 import React, {useState, useCallback} from 'react';
-import {Button, Card, Heading, ButtonGroup} from '@shopify/polaris';
+import {Button, ButtonGroup} from '@shopify/polaris';
 import logo from 'icons/logo.png';
 import example from '../example.png';
 
 import './Login.css';
 import LoginModal from './LoginModal/LoginModal';
+import RegisterModal from './RegisterModal/RegisterModal';
 
 export default function Login() {
-  const [showModal, setShowModal] = useState(false);
-  const toggleShowModal = useCallback(() => setShowModal(!showModal), [showModal]);
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  const toggleShowLoginModal = useCallback(() => setShowLoginModal(!showLoginModal), [showLoginModal]);
+
+  const [showRegisterModal, setShowRegisterModal] = useState(false);
+  const toggleShowRegisterModal = useCallback(() => setShowRegisterModal(!showRegisterModal), [showRegisterModal]);
 
   return (
     <div className='login'>
@@ -20,18 +24,19 @@ export default function Login() {
       <div className="main-page">
         <div className="text-content">
           <div className="intro">Keep an ease of mind</div>
-          <h1 className="main-content">One Fridge, Two Fridge, Shared Fridge, Memofy Tracks it all. </h1>  
+          <h1 className="main-content">One Fridge, Two Fridge, Shared Fridge, Memofy Tracks it all.</h1>
           <div className="additional-content">Let Memofy keep track of your food, so you can focus on enjoying it. </div>
           <ButtonGroup fullWidth>
-              <Button primary onClick={toggleShowModal} fullWidth={false}>Login</Button>
-              <Button outline onClick={toggleShowModal}>Register</Button>
+            <Button primary onClick={toggleShowLoginModal} fullWidth={false}>Login</Button>
+            <Button outline onClick={toggleShowRegisterModal}>Register</Button>
           </ButtonGroup>
         </div>
         <div className="example-content">
           <img src={example} className="app-example" alt={'Not found'} />
         </div>
       </div>
-      <LoginModal active={showModal} handleChange={toggleShowModal} />
+      <LoginModal active={showLoginModal} handleChange={toggleShowLoginModal} />
+      <RegisterModal active={showRegisterModal} handleChange={toggleShowRegisterModal} />
     </div>
   );
 }
