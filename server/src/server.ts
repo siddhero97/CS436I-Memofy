2ffabdc20d8bfd98de8312cd547d97d30d4789dd
Sidd <siddhero97@gmail.com>
2020-06-30 17:29:23 +0530

    Added Schemas
    Within the mongo function I have a set up a connection to the MemofyDatabse. The UserModel maps to the User Schema and updates the Users collection. The Item Model maps to the Item Schema and updates the Items collection.

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,9 +3,19 @@ import compression from 'compression';
 import cors from 'cors';
 import {PORT} from './utils/constants';
 import {BookRoutes} from './components/books';
-
+import {MongoClient} from 'mongodb';
+import {connect, connection, Document, Model, model, Schema, Types} from 'mongoose';
+let ObjectId = Schema.Types.ObjectId;
+let String = Schema.Types.String;
+let Number = Schema.Types.Number;
+let Date  = Schema.Types.Date;
+// import ObjectId from 'Schema.Types.ObjectId';
+// import ObjectId = module
 export default class Server {
   public app: express.Application;
+  private UserModel: Model<Document> | undefined;
+  private ItemModel: Model<Document> | undefined;
+
 
   constructor() {
     this.app = express();
@@ -34,6 +44,38 @@ export default class Server {
   }
 
   private mongo(): void {
-    // initialize mongodb connection here
+    // let MongoClient = require('mongodb').MongoClient;
+    const uri = "mongodb+srv://memofy:hireUsNow@cluster0.ovue1.mongodb.net/MemofyDatabase?retryWrites=true&w=majority";
+    connect(uri, {useNewUrlParser: true})
+        .then(r => {
+              console.log("Connection Is Successful");
+              this.UserModel = model("User", new Schema<any>({id: ObjectId, email: String, password:  String, items: [ObjectId] }), "Users");
+              this.ItemModel = model("Item", new Schema<any>({id: ObjectId, user_id: ObjectId, count: Number, name: String, category: String, icon: String, expiryDate: [Date]}), "Items");
+              //   let userModel = new this.UserModel({
+              //   email: "Bob goes sledding"// assign the _id from the our author Bob. This ID is created by default!
+              // });
+              // userModel.save().then(r => {
+              //     console.log("User Addded Successfully");
+              // }).catch(e => {
+              //   console.error("User Schema Not working");
+              // });
+              //
+              //   let itemModel = new this.ItemModel({
+              //       name: "An Item"// assign the _id from the our author Bob. This ID is created by default!
+              //   });
+              //   itemModel.save().then(r => {
+              //       console.log("Item Addded Successfully");
+              //   }).catch(e => {
+              //       console.error("Item Not working");
+              //   })
+        }
+
+        )
+        .catch(e => {
+          console.error("Connection Failed", e);
+        })
+    //Get the default connection
+
+//Bind connection to error event (to get notification of connection errors)
   }
 }
