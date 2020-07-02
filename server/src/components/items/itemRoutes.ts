eb2ce7427165c56c7421f06d7afc014491a73e31
Vincent Chiang <vincentchiang1@gmail.com>
2020-07-01 18:07:41 -0700

    item schema and routing set up for get, put, post, delete

---
+++ server/src/components/items/itemRoutes.ts
@@ -0,0 +1,19 @@
+import {Router} from 'express';
+import {ItemService} from './services';
+
+export default class BookRoutes {
+  public router: Router;
+  public itemService: ItemService = new ItemService();
+
+  constructor() {
+    this.router = Router();
+    this.routes();
+  }
+
+  routes() {
+    this.router.get('/getItems', this.itemService.getItem);
+    this.router.post('/createItem', this.itemService.postItem);
+    this.router.put('/editItem', this.itemService.putItem);
+    this.router.delete('/deleteItem', this.itemService.deleteItem);
+  }
+}
