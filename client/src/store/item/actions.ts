b9b26209fe230ec48f506c51755a921744e8b22d
john <likisongproduct@gmail.com>
2020-07-07 21:12:50 -0700

    working client side addItem

--- client/src/store/item/actions.ts
+++ client/src/store/item/actions.ts
@@ -1,10 +1,25 @@
 import {Item, ItemActionTypes, ADD_ITEM, DELETE_ITEM} from './types';
+const axios = require('axios');
 
-export function addItem(newItem: Item): ItemActionTypes {
-  return {
-    type: ADD_ITEM,
-    payload: newItem,
-  };
+// not sure what the type should be so set it to any for now
+export const addItem = (newItem: Item) : any => async (
+  dispatch: any) => {
+  try { 
+    await axios.post('http://localhost:4000/items/createItem', {
+      user_id: "1",
+      count: newItem.count,
+      name: newItem.name,
+      category: newItem.category,
+      icon: newItem.icon,
+      expiryDate: newItem.expiry
+    });
+    dispatch({
+      type: ADD_ITEM,
+      payload: newItem,
+    })
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export function deleteItem(name: string): ItemActionTypes {
