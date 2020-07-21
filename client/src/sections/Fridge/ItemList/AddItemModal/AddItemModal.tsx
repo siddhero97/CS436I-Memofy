acc5c53a200eb3f18d021480f8b27b7a517642ef
Vincent Chiang <vincentchiang1@gmail.com>
2020-07-21 16:31:55 -0700

    add remaining item fields into add item modal

--- client/src/sections/Fridge/ItemList/AddItemModal/AddItemModal.tsx
+++ client/src/sections/Fridge/ItemList/AddItemModal/AddItemModal.tsx
@@ -1,6 +1,6 @@
 import React, {useState, useCallback} from 'react';
 import {useDispatch} from 'react-redux';
-import {Button, Modal, TextField, Card} from '@shopify/polaris';
+import {Button, Modal, TextField, Card, DatePicker, Select} from '@shopify/polaris';
 import {thunkAddItem} from 'store/item/actions';
 
 export default function AddItemModal() {
@@ -8,6 +8,29 @@ export default function AddItemModal() {
 
   const [showModal, setShowModal] = useState(false);
   const [value, setValue] = useState('');
+  const [name, setName] = useState('');
+  const [fridge, setFridge] = useState('My fridge');
+  const fridges = [
+    {label: 'My fridge', value: 'today'},
+    {label: 'Home fridge', value: 'yesterday'},
+    {label: 'Dorm fridge', value: 'lastWeek'},
+  ];
+  const [category, setCategory] = useState('Categories');
+  const categories = [
+    {label: 'Meats', value: 'today'},
+    {label: 'Fruits', value: 'yesterday'},
+    {label: 'Veggies', value: 'lastWeek'},
+  ];
+  const [count, setCount] = useState('0');
+  const [{month, year}, setDate] = useState({
+    month: 6,
+    year: 2020,
+  });
+  const [selectedDates, setSelectedDates] = useState({
+    start: new Date('Tues Jul 22 2020 00:00:00 GMT-0500 (EST)'),
+    end: new Date('Tues Jul 22 2020 00:00:00 GMT-0500 (EST)'),
+  });
+  const [icon, setIcon] = useState('');
 
   const toggleShowModal = useCallback(() => {
     setShowModal(!showModal);
@@ -16,8 +39,8 @@ export default function AddItemModal() {
   const handleSubmit = useCallback(() => {
     const newItem = {
       fridgeId: 1234,
-      count: 1,
-      name: value,
+      count: +count,
+      name: name,
       // this will come from the database once setup
       category: 'Fruits',
       icon: 'url',
@@ -31,6 +54,15 @@ export default function AddItemModal() {
   const handleTextChange = useCallback((newValue) => {
     setValue(newValue);
   }, []);
+  const handleNameChange = useCallback((name) => setName(name), []);
+  const handleFridgeChange = useCallback((fridge) => setFridge(fridge), []);
+  const handleCategoryChange  = useCallback((category) => setCategory(category), []);
+  const handleCountChange = useCallback((count) => setCount(count), []);
+  const handleMonthChange = useCallback(
+    (month, year) => setDate({month, year}),
+    [],
+  );
+  const handleIconChange = useCallback((icon) => setIcon(icon), []);
 
   return (
     <Card.Section title="Can't find your item?">
@@ -52,7 +84,14 @@ export default function AddItemModal() {
         ]}
       >
         <Modal.Section>
-          <TextField label="Food name" value={value} onChange={handleTextChange} />
+          <TextField label="Food name" value={name} onChange={handleNameChange} />
+          <Select label="Fridge" options={fridges} onChange={handleFridgeChange} value={fridge} />
+          <Select label="Category" options={categories} value={category} onChange={handleCategoryChange} />
+          <TextField label="Count" type="number" value={count} onChange={handleCountChange} />
+          <header>Expiry date</header>
+          <DatePicker month={month} year={year} onChange={setSelectedDates} onMonthChange={handleMonthChange}
+          selected={selectedDates} />
+          <TextField label="Icon" value={icon} onChange={handleIconChange} />
         </Modal.Section>
       </Modal>
     </Card.Section>
