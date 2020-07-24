0a4ae9409cc468fe93940ef9c2b77a7bacfaf92d
Jaehun Song <jaehun.song@mac-jaehun.song>
2020-07-24 01:46:40 -0700

    working redux and add fridge name ui connected to fridge post

--- server/src/components/fridges/services/createFridgeService.ts
+++ server/src/components/fridges/services/createFridgeService.ts
@@ -4,10 +4,12 @@ import {createFridge} from '../DALs';
 export default class CreateFridgeService {
   public async execute(req: Request, res: Response): Promise<void> {
     try {
+      console.log(req.body)
       const fridge = await createFridge(req.body);
 
       res.status(200).json({fridge});
     } catch (error) {
+      console.log(error);
       res.status(500).json({error});
     } 
   }
--- server/src/components/fridges/services/fetchFridgeService.ts
+++ server/src/components/fridges/services/fetchFridgeService.ts
@@ -4,9 +4,9 @@ import {findFridge} from '../DALs';
 export default class FetchFridgeService {
   public async execute(_req: Request, res: Response): Promise<void> {
     try {
-      const items = await findFridge();
+      const fridges = await findFridge();
 
-      res.status(200).json({items});
+      res.status(200).json({fridges});
     } catch (error) {
       res.status(500).send(error);
     }
