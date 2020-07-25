29d04783a1e8caa5ffe0d33802e6be1f239237fb
Siddhartha Gupta <siddhero97@gmail.com>
2020-07-25 13:50:23 +0530

    Updated Profile Page
    Fixed all lint errors.

--- client/src/sections/Fridge/Header/Header.tsx
+++ client/src/sections/Fridge/Header/Header.tsx
@@ -2,23 +2,15 @@ import React from 'react';
 import {ProfileMajorMonotone} from '@shopify/polaris-icons';
 import {Button, Heading} from '@shopify/polaris';
 import {Navbar} from './components';
-
 import logo from 'icons/logo.png';
-
 import './Header.css';
-import Profile from 'sections/Profile';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { userInfo } from 'os';
-
+import {useHistory} from 'react-router-dom';
 export default function Header() {
-  const dispatch = useDispatch();
   const history = useHistory();
 
   const handleProfileClick = () => {
   // console.log(useSelectorS(userInfo));
-  history.push('./profile');
-  };
+    history.push('./profile');};
   return (
     <div className='header'>
       <div className='header-title'>
@@ -33,7 +25,4 @@ export default function Header() {
         }/>
       </div>
     </div>
-  );
-
-  
-}
+  );}
--- client/src/sections/index.ts
+++ client/src/sections/index.ts
@@ -1,3 +1,3 @@
 export {default as Fridge} from './Fridge';
 export {default as Login} from './Login';
-export {default as Profile} from "./Profile"
+export {default as Profile} from "./Profile";
